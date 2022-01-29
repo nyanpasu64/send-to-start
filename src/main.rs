ec6a27e2bb47f6a4d833844f489a71e5b33e81be
nyanpasu64 <nyanpasu64@tuta.io>
2022-01-28 16:15:32 -0800

    subcommands

--- src/main.rs
+++ src/main.rs
@@ -5,63 +5,42 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
-
-    /// Sets a custom config file
-    #[clap(short, long, parse(from_os_str), value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[clap(short, long, parse(from_occurrences))]
-    debug: usize,
-
     #[clap(subcommand)]
     command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[clap(short, long)]
-        list: bool,
+    /// Install a "Send to start" item in the "Send to" menu.
+    Install,
+
+    /// Create a Start Menu shortcut from a .exe file.
+    Create {
+        exe: PathBuf,
     },
+
+    /// Remove the "Send to start" item from the "Send to" menu.
+    Uninstall,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {}", name);
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level app
     match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
+        Some(Commands::Install) => {
+            eprintln!("install to \"Send to\"");
         }
-        None => {}
+        Some(Commands::Create {ref exe}) => {
+            eprintln!("create shortcut to {}", exe.display());
+        }
+        Some(Commands::Uninstall) => {
+            eprintln!("uninstall");
+        }
+        None => {
+            eprintln!("Install \"Send to start\"?");
+        }
     }
 
     // Continued program logic goes here...
