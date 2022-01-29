c3f11a92e9bc856187007f942febefa3932c4995
nyanpasu64 <nyanpasu64@tuta.io>
2022-01-29 00:07:39 -0800

    progress

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,7 @@ struct Cli {
 fn get_path(folder_id: GUID) -> Result<PathBuf> {
     use std::os::windows::prelude::OsStringExt;
     unsafe {
-        let path_ptr: PWSTR = SHGetKnownFolderPath(&folder_id, KF_FLAG_DEFAULT as u32, HANDLE(0))
-            .context("Cannot find path")?;
+        let path_ptr: PWSTR = SHGetKnownFolderPath(&folder_id, KF_FLAG_DEFAULT as u32, HANDLE(0))?;
         let path_ref: &[u16] = slice::from_raw_parts(path_ptr.0, lstrlenW(path_ptr) as usize);
         let path_copy: PathBuf = OsString::from_wide(path_ref).into();
         CoTaskMemFree(path_ptr.0 as *mut c_void);
@@ -39,17 +38,17 @@ fn create_lnk(link: &Path, target: &Path) -> Result<()> {
     unsafe {
         let shell_link: IShellLinkW =
             CoCreateInstance(&Shell::ShellLink, None, Com::CLSCTX_INPROC_SERVER)
-                .context("Cannot load .lnk file library")?;
+                .context("loading .lnk file library")?;
         shell_link
             .SetPath(target.as_os_str())
-            .context("Cannot set .lnk path")?;
+            .with_context(|| format!("setting .lnk path to \"{}\"", target.display()))?;
 
         let persist_file: IPersistFile = shell_link
             .cast::<IPersistFile>()
-            .context("Cannot load .lnk saving interface")?;
+            .context("loading .lnk saving interface")?;
         persist_file
             .Save(link.as_os_str(), true)
-            .context("Cannot write .lnk file")?;
+            .with_context(|| format!("writing .lnk file to \"{}\"", link.display()))?;
     }
 
     Ok(())
@@ -58,17 +57,23 @@ fn create_lnk(link: &Path, target: &Path) -> Result<()> {
 fn main() -> Result<()> {
     unsafe {
         CoInitializeEx(std::ptr::null(), Com::COINIT_APARTMENTTHREADED)
-            .context("Failed to initialize COM")?;
+            .context("failed to initialize COM")?;
     }
 
     let cli = Cli::parse();
 
     if let Some(exe) = cli.exe {
-        let start_menu = get_path(Shell::FOLDERID_StartMenu).context("for Start menu")?;
+        let start_menu =
+            get_path(Shell::FOLDERID_StartMenu).context("failed to find Start menu path")?;
 
         let mut folder = start_menu;
         folder.push("Shortcuts");
-        create_dir_all(&folder).context("Failed to create Shortcuts folder")?;
+        create_dir_all(&folder).with_context(|| {
+            format!(
+                "failed to create Shortcuts folder at \"{}\"",
+                folder.display()
+            )
+        })?;
 
         let mut shortcut = folder;
         // See https://users.rust-lang.org/t/append-an-additional-extension/23586
@@ -78,8 +83,8 @@ fn main() -> Result<()> {
         let abs_exe = if exe.is_absolute() {
             exe
         } else {
-            let mut abs_exe = std::env::current_dir()
-                .context("Cannot find current directory for relative path")?;
+            let mut abs_exe =
+                std::env::current_dir().context("failed to resolve relative EXE path")?;
             abs_exe.push(exe);
             abs_exe
         };
@@ -89,16 +94,19 @@ fn main() -> Result<()> {
             shortcut.display(),
             abs_exe.display(),
         );
-        create_lnk(&shortcut, &abs_exe).context("creating shortcut")?;
+        create_lnk(&shortcut, &abs_exe).context("failed to create app shortcut")?;
     } else {
-        let send_to = get_path(Shell::FOLDERID_SendTo).context("for Send To")?;
-        let exe_name = current_exe().context("Cannot find program name")?;
+        let send_to = get_path(Shell::FOLDERID_SendTo).context("failed to find Send To path")?;
+        let mut shortcut = send_to;
+        shortcut.push("Start (create shortcut).lnk");
+
+        let exe_name = current_exe().context("failed to locate program for Send To")?;
         eprintln!(
             "install shortcut at \"{}\" to \"{}\"",
-            send_to.display(),
+            shortcut.display(),
             exe_name.display()
         );
-        create_lnk(&send_to, &exe_name).context("installing to Send To")?;
+        create_lnk(&shortcut, &exe_name).context("failed to install to Send To")?;
     }
 
     Ok(())
